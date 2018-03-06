2f32f6c3ef28f02c8408818de65a11226ab2dcab
korszulak <karol.orszulak@gmail.com>
2018-03-06 13:00:27 -0600

    Added more error content

--- JS/survey.js
+++ JS/survey.js
@@ -6,33 +6,42 @@
 
 function validateForm(event) {
 	var school = document.getElementById("school");
+	var eSchool = document.getElementById("eSchool");
 	var grade = document.getElementById("grade");
+	var eGrade = document.getElementById("eGrade");
 	var age = document.getElementById("age");
+	var eAge = document.getElementById("eAge");
 	var gender = document.querySelectorAll("input[name='gender']");
 	var error = document.getElementById("error");
 	
 	// Validate school input
 	if (school.value.trim().length === 0) {
 		school.style.borderColor = "red";
+		eSchool.style.display = "block";
 		event.preventDefault();
 	} else {
 		school.style.borderColor = "white";
+		eSchool.style.display = "none";
 	}
 	
 	// Validate grade input
 	if (grade.value.trim().length === 0) {
-		grade.style.borderColor = "red";	
+		grade.style.borderColor = "red";
+		eGrade.style.display = "block";	
 		event.preventDefault();
 	} else {
 		grade.style.borderColor = "white";
+		eGrade.style.display = "none";
 	}
 	
 	// Validate age input
 	if (age.value.trim().length === 0 || isNaN(age.value) === true) {
 		age.style.borderColor = "red";
+		eAge.style.display = "block";	
 		event.preventDefault();
 	} else {
 		age.style.borderColor = "white";
+		eAge.style.display = "none";
 	}
 	
 	// Validate gender
