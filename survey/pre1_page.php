aed6929ba3d45e27db091d2d341101c9e3ec30d5
Ali Kazmi <alihkazmi@lewisu.edu>
2018-03-08 00:42:55 +0000

    added pre1_page.php

---
+++ survey/pre1_page.php
@@ -0,0 +1,40 @@
+<?php 
+$host="localhost";
+$user="kazmi01";
+$pass="";
+$db="Survey";
+$connection=mysqli_connect($host,$user,$pass,$db);
+if($connection->connect_error){
+    die("connection failed:". $conn->connect_error);
+}
+
+$SchoolName= $_POST['school'];
+$GradeLevel= $_POST['grade'];
+$Age= $_POST['age'];
+$Gender= $_POST['gender'];
+if(!empty($SchoolName)&&!empty($GradeLevel)&&!empty($Age)&&!empty($Gender)){
+    $sql="INSERT INTO Users(SchoolName,GradeLevel,Age,Gender) VALUES ('$SchoolName','$GradeLevel','$Age','$Gender')";
+    $connection->query($sql);
+    
+}
+else{
+    header("location:index.html");
+}
+$connection->close();
+?>
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">		
+		<title>H2O</title>
+		<link rel="stylesheet" type="text/css" href="../CSS/survey.css" />
+	</head>
+	<body>
+		<div id="wrapper-header">
+			<div id="header">
+				<img id="logo" src="http://hearthavenoutreach.org/wp-content/uploads/2017/08/H2O-logo-PNG-clear-background-white-600.png" alt="logo" />
+			</div>
+		</div>
+	</body>
+</html>
