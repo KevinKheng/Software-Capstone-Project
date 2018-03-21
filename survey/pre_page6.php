3412c6d9ed51cbeeadbdbb1a9567ac36b80a3074
Ahmad dahshan <ahmaddahshan@Ahmads-MacBook-Air.local>
2018-03-21 01:22:11 -0500

    added thank you page

---
+++ survey/pre_page6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">		
+		<title>H2O | Pre Survey</title>
+		<link rel="stylesheet" type="text/css" href="css/survey.css">
+		<link rel="stylesheet" type="text/css" href="css/surveyPageStyle.css">
+	</head>
+	<body>
+		<div id="wrapper-header">
+			<div id="header">
+				<img id="logo" src="http://hearthavenoutreach.org/wp-content/uploads/2017/08/H2O-logo-PNG-clear-background-white-600.png" alt="logo" />
+			</div>
+		</div>
+		<p>Thank You for Filling Out our Survey</p>
+		<p id="idNum">Your login ID number is</p>
+		<script type="text/javascript" src="scripts/backbtn.js"></script>
+	</body>
+</html>
